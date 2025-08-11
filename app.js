03d029d7d16902c7c32b2845880987db66e1be00
VINICIUS JULIO DE OLIVEIRA <25132898@dsn127m1249388.cfp127aluno.local>
2025-08-11 14:16:24 -0300

    cd

--- app.js
+++ app.js
@@ -8,3 +8,30 @@ function trocarImg () {
 }
 
 botaoTrocarImagem.addEventListener('click', trocarImg)
+
+
+
+
+
+
+const botaoAuto = document.getElementById('auto')
+const slides = document.querySelectorAll('.slide')
+let index = 0
+let intervalo = null
+
+function mostrarSlide(i) {
+    slides.forEach(slides => slides.classList.remove('active'))
+    slides[i].classList.add('active')
+  }
+function iniciarLoop() {
+    if (intervalo) return
+
+    mostrarSlide(index)
+
+    intervalo = setInterval(() => {
+        index = (index + 1) % slides.length
+        mostrarSlide(index)
+    }, 3000)
+
+}
+botaoAuto.addEventListener('click', iniciarLoop)
